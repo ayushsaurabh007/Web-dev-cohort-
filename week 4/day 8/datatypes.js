ae0400b660784f90727c9cedc9a8c8ec5d3e252c
ayushsaurabh007 <117847021+ayushsaurabh007@users.noreply.github.com>
2025-04-01 16:53:37 +0530

    Create datatypes.js

---
+++ week 4/day 8/datatypes.js
@@ -0,0 +1,30 @@
+//all the datatypes in javascrit
+
+// ------------------------PRIMITIVE DATA TYPES----------------------
+//string
+let name = "Ayush Saurabh" ;
+
+//number 
+let rank = 1;
+
+//boolean
+let is_goku_strongest = true;
+
+//null
+let temp = null; //is we dont know temp and is kept 0 then it will be very cold
+
+//undefined 
+let internship ;  // baad me dekhenege 
+
+//-----------------------NON PRIMITIVE DATA TYPES-----------------------
+
+//array
+let car = ["lamborghini" , "koenigsegg" , "bugatti"];
+
+//object 
+let myobj ={
+    name : "ayush",
+    age : 19 , 
+    subject : "web development",
+    year : 2025
+}
